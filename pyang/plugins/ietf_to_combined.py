9a23b187bef4a5f285c927d23dd85861389bd387
rwilton <rwilton@cisco.com>
2017-04-05 22:28:21 +0100

    add option to remove state nodes

--- pyang/plugins/ietf_to_combined.py
+++ pyang/plugins/ietf_to_combined.py
@@ -37,8 +37,12 @@ class IetfModelPlugin(plugin.PyangPlugin):
         optparser.add_option("--ietf-split-to-combined-tree", dest="ietf_split_to_combined",
                              action="store_true",
                              help="From an existing IETF split config/state tree, generate a single combined tree")
+        optparser.add_option("--remove-state-nodes", dest="ietf_remove_state_nodes",
+                             action="store_true",
+                             help="Remove state nodes, rather than marking them deprecated")
 
     def post_validate_ctx(self, ctx, modules):
+        IetfModelPlugin.remove_state = ctx.opts.ietf_remove_state_nodes
         if ctx.opts.ietf_split_to_combined:
             # Run the plugin here.
             module = modules[0]
@@ -149,59 +153,99 @@ def convert_stmt(config_stmt, state_stmt):
         else:
             if (s.keyword != 'config'):
                 # Copy the state stmt over to the config
-                config_stmt.substmts.append(s)
-                fixup_stmt(s)
-                if s.keyword in {"container", "leaf", "leaf-list", "list", "anyxml", "anydata", "choice"}:
-                    add_substmt_canonical(s, Statement(s.top, s, s.pos, 'config', 'false'))
+                copied_stmt = s.copy()
+                config_stmt.substmts.append(copied_stmt)
+                fixup_stmt(copied_stmt)
+                if copied_stmt.keyword in {"container", "leaf", "leaf-list", "list", "anyxml", "anydata", "choice"}:
+                    add_substmt_canonical(copied_stmt,
+                                          Statement(copied_stmt.top, copied_stmt, copied_stmt.pos,
+                                                    'config', 'false'))
                 
+def mark_stmt_deprecated(state_stmt):
+    # Iterate the state tree
+    def can_have_status(stmt):
+        return (stmt.keyword in ['action', 'anydata', 'anyxml', 'augment', 'bits',
+                                 'case', 'choice', 'container', 'enum', 'extension',
+                                 'feature', 'grouping', 'identity', 'leaf', 'list',
+                                 'leaf-list', 'notification', 'rpc', 'typedef', 'uses'])  
+    if (can_have_status(state_stmt)):
+        status_current = True
+        for substmt in state_stmt.substmts:
+            if (substmt.keyword == 'status' and substmt.arg != "current"):
+                status_current = False
+        if (status_current):
+            add_substmt_canonical(state_stmt,
+                                  Statement(state_stmt.top,
+                                            state_stmt,
+                                            state_stmt.pos,
+                                            'status',
+                                            'deprecated'))
+    # Recurse down children state statements.
+    for substmt in state_stmt.substmts:
+        mark_stmt_deprecated(substmt)
+
+
 # Convert an IETF module, assume "-state" is a top level split.
-def convert_module(ctx, stmt):
-    if ctx.opts.yang_remove_unused_imports and stmt.keyword == 'import':
-        for p in stmt.parent.i_unused_prefixes:
-            if stmt.parent.i_unused_prefixes[p] == stmt:
+def convert_module(ctx, m_stmt):
+    if ctx.opts.yang_remove_unused_imports and m_stmt.keyword == 'import':
+        for p in m_stmt.parent.i_unused_prefixes:
+            if m_stmt.parent.i_unused_prefixes[p] == m_stmt:
                 return
 
-    if util.is_prefixed(stmt.raw_keyword):
-        (prefix, identifier) = stmt.raw_keyword
+    if util.is_prefixed(m_stmt.raw_keyword):
+        (prefix, identifier) = m_stmt.raw_keyword
         keyword = prefix + ':' + identifier
     else:
-        keyword = stmt.keyword
+        keyword = m_stmt.keyword
 
     if keyword in ['module', 'submodule']:
-        # Change the module name.
-        module_name = stmt.arg
-        stmt.arg = module_name + '-2'
+        # Don't change the module name, but could update the revision number?
+        #module_name = m_stmt.arg
+        #m_stmt.arg = module_name + '-2'
         
         # Look for config/state containers to convert.
-        if len(stmt.substmts) != 0:
-            substmts = grammar.sort_canonical(stmt.keyword, stmt.substmts)
-            for substmt in substmts:
+        if len(m_stmt.substmts) != 0:
+            m_substmts = grammar.sort_canonical(m_stmt.keyword, m_stmt.substmts)
+            for substmt in m_substmts:
                 if substmt.keyword in ['namespace', 'belongs-to']:
                     substmt.arg = substmt.arg + '-2'
                 
+                # Remove the interface state ref?
+                # Or, is this even appropriate.
                 if substmt.keyword == 'typedef' and substmt.arg == 'interface-state-ref':
-                    stmt.substmts.remove(substmt)
+                    m_stmt.substmts.remove(substmt)
                 
                 # Fix up import references to point to the combined modules.
-                if (substmt.keyword in ['import', 'include']
-                    and substmt.arg.startswith("ietf-")
-                    and substmt.arg not in ["ietf-yang-types","ietf-inet-types"]):
-                    substmt.arg = substmt.arg + "-2"
+                #if (substmt.keyword in ['import', 'include']
+                #    and substmt.arg.startswith("ietf-")
+                #    and substmt.arg not in ["ietf-yang-types","ietf-inet-types"]):
+                #    substmt.arg = substmt.arg + "-2"
                 
                 # Handle top level config/state containers.
                 if is_config_container(substmt):
-                    for s in substmts:
-                        if (substmt.arg + "-state" == s.arg and is_state_container(s)):
+                    for s in m_substmts:
+                        if (s.arg == (substmt.arg + "-state") and is_state_container(s)):
                             convert_stmt(substmt, s)
-                            stmt.substmts.remove(s)
+                            
+                            # Either remove the m_substmts, or mark it as status deprecated.
+                            if IetfModelPlugin.remove_state:
+                                m_stmt.substmts.remove(s)
+                            else:
+                                mark_stmt_deprecated(s)
+                
                             
                 # Handle top level augmentations of config/state containers.
                 if is_state_augmentation(substmt):
-                    cfg_augment = [c for c in substmts if matches_cfg_augment(c, substmt)] 
+                    cfg_augment = [c for c in m_substmts if matches_cfg_augment(c, substmt)] 
                     if cfg_augment:
                         cfg_stmt = cfg_augment[0]
                         convert_stmt(cfg_stmt, substmt)
-                        stmt.substmts.remove(substmt)
+                        
+                        # Either remove the m_substmts, or mark it as status deprecated.
+                        if IetfModelPlugin.remove_state:
+                            m_stmt.substmts.remove(substmt)
+                        else:
+                            mark_stmt_deprecated(substmt)
                     else:
                         # If it is just a state augmentation then just rename the augmentation.
                         cfg_arg = matching_cfg_augment(substmt.arg)
